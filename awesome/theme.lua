ddb4eb60fe5a3bb0269508f047de6f378f897623
Joachim Pileborg <joachimp@axis.com>
2011-10-12 15:47:12 +0200

    Changed colors in theme

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -13,4 +13,11 @@ local success, theme = pcall(function() return dofile(orig_theme_name) end)
 theme.font = "monospace 8"
 theme.wallpaper_cmd = { 'xsetroot -solid "#333333"' }
 
+theme.border_width  = "1"
+
+theme.border_focus  = "#6F6FBF"
+
+theme.titlebar_bg_focus  = "#6F6FBF"
+theme.titlebar_bg_normal = "#3F3F3F"
+
 return theme
